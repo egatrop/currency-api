0036b36492d3f17a60ec4b50262d2f1701a4bc7f
Yahor Ivanou <yahor.ivanou@quando.com>
2021-02-01 09:51:51 +0100

    fix: remove unused property from AppProperties

--- src/main/java/com/yivanou/currencyconverter/config/AppProperties.java
+++ src/main/java/com/yivanou/currencyconverter/config/AppProperties.java
@@ -3,19 +3,12 @@ package com.yivanou.currencyconverter.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Set;
-
 @Configuration
 @ConfigurationProperties(prefix = "spring")
 public class AppProperties {
 
-    private Set<String> currencies;
     private String ecbUrl;
     private String xeUrl;
-
-    public void setCurrencies(Set<String> currencies) {
-        this.currencies = currencies;
-    }
 
     public void setEcbUrl(String ecbUrl) {
         this.ecbUrl = ecbUrl;
